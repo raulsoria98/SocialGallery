a9a22689aca0f3806d3e3cb8f5ac0fe51954da10
Raúl Soria <raulsogo@hotmail.com>
2023-04-13 17:52:07 +0200

    register DTO not need role

--- server/src/dto/schemas/register.schema.js
+++ server/src/dto/schemas/register.schema.js
@@ -3,8 +3,7 @@ import generateDTOSchema from '#Utils/generateDTOSchema.js'
 const RegisterDTOSchema = generateDTOSchema([
   { name: 'email', required: true },
   { name: 'password', required: true },
-  { name: 'name', required: true },
-  { name: 'role', required: true }
+  { name: 'name', required: true }
 ])
 
 export default RegisterDTOSchema
